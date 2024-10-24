cd61acecfb185e4fee3b3ff51cca06040b3716e7
Saidul Islam <saidulislam197120@gmail.com>
2024-10-24 23:21:41 +0600

    Fix some issues

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -4,7 +4,6 @@ const  getCroppedImageUrl = (url: string) => {
     if (!url) return defaultImage;
     const target = 'media/';
     const index = url.indexOf(target) + target.length;
-    console.log(target, index);
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
 }
 
