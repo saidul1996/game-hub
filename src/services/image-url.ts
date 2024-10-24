821e106a8b2b77946da83297095c36655ec4749c
Saidul Islam <saidulislam197120@gmail.com>
2024-10-24 23:15:24 +0600

    Handling games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import defaultImage from '../assets/no-image.png'
+
 const  getCroppedImageUrl = (url: string) => {
-    if (!url) return '';
+    if (!url) return defaultImage;
     const target = 'media/';
     const index = url.indexOf(target) + target.length;
     console.log(target, index);
